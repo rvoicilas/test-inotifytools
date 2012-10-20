1b44c1edd94864ee1dd9c63c84bd0c3861e371f4
Radu Voicilas <radu.voicilas@gmail.com>
2012-10-20 20:57:00 +0300

    Tests for --include and --includei switches

--- test_inotifywait.py
+++ test_inotifywait.py
@@ -32,8 +32,9 @@ class TestInotifywait(unittest.TestCase):
     def setUpClass(cls):
         cls._ensure_inotifywait_installed()
 
-    def _make_temp_file(self):
-        handle, path = tempfile.mkstemp()
+    def _make_temp_file(self, prefix=None):
+        kwargs = prefix is not None and {'prefix': prefix} or {}
+        handle, path = tempfile.mkstemp(**kwargs)
         os.fdopen(handle).close()
         return path
 
@@ -60,9 +61,8 @@ class TestInotifywait(unittest.TestCase):
         def _write_to_file():
             # the write might happen before inotifywait notices it
             time.sleep(0.001)
-            fd = open(self._testfile, 'w')
-            fd.write(' '.join(cmd))
-            fd.close()
+            with open(self._testfile, 'w') as fd:
+                fd.write(' '.join(cmd))
 
         _write_to_file()
         stdout, _ = proc.communicate()
@@ -88,3 +88,82 @@ class TestInotifywait(unittest.TestCase):
 
         self.assertEqual(0, proc.returncode)
         self.assertIn("MOVE_SELF", stdout)
+
+    def test_include(self):
+        """Make sure that only events for files specified with
+        --include are processed by inotifywait.
+        """
+
+        sut = self._make_temp_file(prefix='include')
+
+        cmd = [self._inotify,
+               "--quiet",
+               "--event", "OPEN",
+               "--timeout", "5",
+               "--include", "include.*?",
+               self._testfile, sut]
+
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+        def _open_file(fpath):
+            time.sleep(0.001)
+            open(fpath)
+
+        _open_file(self._testfile)
+        _open_file(sut)
+
+        stdout, _ = proc.communicate()
+
+        self.assertEqual(0, proc.returncode)
+
+        expected = '{0} OPEN'.format(sut)
+        self.assertEqual(expected, stdout.strip())
+
+        os.remove(sut)
+
+    def test_includei(self):
+        """Make sure that only events for files specified with
+        --includei are processed by inotifywait.
+        """
+
+        sut = self._make_temp_file(prefix='includei')
+
+        cmd = [self._inotify,
+               "--quiet",
+               "--event", "OPEN",
+               "--timeout", "5",
+               "--includei", "INCLUDEI.*?",
+               self._testfile, sut]
+
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+        def _open_file(fpath):
+            time.sleep(0.001)
+            open(fpath)
+
+        _open_file(self._testfile)
+        _open_file(sut)
+
+        stdout, _ = proc.communicate()
+
+        self.assertEqual(0, proc.returncode)
+
+        expected = '{0} OPEN'.format(sut)
+        self.assertEqual(expected, stdout.strip())
+
+        os.remove(sut)
+
+    def test_include_and_includei_mutually_exclusive(self):
+        cmd = [self._inotify,
+               "--quiet",
+               "--include", "include.*?",
+               "--includei", "INCLUDEI.*?",
+               self._testfile]
+
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+
+        expected = '--include and --includei cannot both be specified.'
+        self.assertFalse(len(stdout) > 0)
+        self.assertEqual(expected, stderr.strip())
